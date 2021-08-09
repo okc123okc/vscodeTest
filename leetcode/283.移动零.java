5a6bcf2dd88015294d867afa7c1e68e2a368ec88
okc <okc123123@126.com>
2021-08-10 00:02:12 +0800

    283

--- leetcode/283.移动零.java
+++ leetcode/283.移动零.java
@@ -7,12 +7,25 @@
 // @lc code=start
 class Solution {
     public void moveZeroes(int[] nums) {
+        // int j = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] != 0){
+        //        int temp = nums[i];
+        //        nums[i] = nums[j]; 
+        //        nums[j++] = temp;
+        //     }
+        // }
+        if (nums == null) {
+            return;
+        }
         int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0){
-               int temp = nums[i];
-               nums[i] = nums[j]; 
-               nums[j++] = temp;
+        for (int i = 0;i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i>j){
+                    nums[j] = nums[i];
+                    nums[i] = 0;
+                }
+                j++;
             }
         }
 
