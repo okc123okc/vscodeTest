35a0da4b5764917e51dd228949aaf3e689027676
okc <okc123123@126.com>
2021-08-09 14:55:33 +0800

    v3.1

---
+++ leetcode/1.两数之和.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for(int j=0;j<nums.length;j++){
+            if(map.containsKey(target-nums[j])&&map.get((target-nums[j]))!=j){
+                return new int[] {j,map.get((target-nums[j]))};
+            }
+        }
+        return new int[] {0};
+
+    }
+}
+// @lc code=end
+
