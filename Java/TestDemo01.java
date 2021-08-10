01be6633bde4f72376bae17250df0c899576e5c9
okc123okc <86590184+okc123okc@users.noreply.github.com>
2021-08-10 14:58:18 +0800

    Update TestDemo01.java

--- Java/TestDemo01.java
+++ Java/TestDemo01.java
@@ -2,6 +2,6 @@
 public class TestDemo01 {
     public static void main(String[] args) {
         System.out.println("我自己改");
-        System.out.println("他人改");
+        System.out.println("再改");
     }
 }
