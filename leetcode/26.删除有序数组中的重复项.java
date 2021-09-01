a06e12365a2fb848d977b3632d44470a912ae96b
okc <okc123123@126.com>
2021-09-02 00:52:11 +0800

    review 26

--- leetcode/26.删除有序数组中的重复项.java
+++ leetcode/26.删除有序数组中的重复项.java
@@ -7,21 +7,18 @@
 // @lc code=start
 class Solution {
     public int removeDuplicates(int[] nums) {
-        //快慢指针解法
         int len = nums.length;
-        //慢 用于不同元素填入的位置
         int slow = 0;
-        //快  用于遍历下标 
         int fast = 1;
-        if (len == 0 || nums == null) return 0;
-        while (fast < len) {
+        if (nums == null || len == 0) return 0;
+        while (fast < len ) {
             if (nums[slow] != nums[fast]) {
-                nums [slow + 1] = nums[fast];
+                nums[slow+1] = nums[fast];
                 slow++;
             }
             fast++;
-        } 
-        return slow + 1;        
+        }       
+        return slow+1;
     }
 }
 // @lc code=end
